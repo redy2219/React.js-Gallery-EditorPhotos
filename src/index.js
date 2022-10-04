2c44240108852027db3841da7c965cfee9fe29db
katya <rodionova@enterego.ru>
2022-10-04 23:37:29 +0300

    initial commit - test component

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import PanelPortal from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const panelPortal = ReactDOM.createRoot(document.getElementById('panelPortal'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
-
+panelPortal.render(
+    <React.StrictMode>
+        <PanelPortal/>
+    </React.StrictMode>)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
